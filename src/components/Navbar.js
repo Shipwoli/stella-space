c136fb7cf987cb7689bdc399118edf33818e6eb0
matomugo60 <matomugo60@gmail.com>
2023-01-24 14:39:55 +0300

    created function navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/picturedays">Nasa Picture of the Day</Link>
+        </li>
+        <li>
+          <Link to="/weathermars">Weather on Planet Mars</Link>
+        </li>
+        <li>
+            <Link to="/marsphotos">Mars Rover Photos</Link>
+        </li>
+        <li>
+            <Link to="/mapasteroids">Map of Asteroids</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
