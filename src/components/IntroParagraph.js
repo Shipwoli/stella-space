b8dd5be928870f795fc245c7ed06a5302a45f326
matomugo60 <matomugo60@gmail.com>
2023-01-25 13:13:04 +0300

    changed introparagraph

--- src/components/IntroParagraph.js
+++ src/components/IntroParagraph.js
@@ -4,12 +4,13 @@ function IntroParagraph () {
     return (
         <div>
             <h1>Stellar Space</h1>
-                <p>Welcome to stella space! This is a simple platform that enables one to display: </p>
+                <p>Welcome to Stellar Space! This is a simple platform that enables one to display: </p>
                 <li>1.Nasa Picture of the day.</li>
                 <li>2.Objects near the earth.</li>
                 <li>3.The weather on planet Mars.</li>
                 <li>4.Mars Rover photos.</li>
                 <li>5.Map of asteroids within space.</li>
+                <p>Scroll down to view the data</p>
         </div>
     );
 }
